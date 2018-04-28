4900191eff136d1454be6f354bb112c2e3320c7e
gullocean <gullocean@gmail.com>
2018-04-28 03:20:50 -0300

    update markup without building

--- server/universal.js
+++ server/universal.js
@@ -13,46 +13,38 @@ const {default: App} = require('./../src/App')
 module.exports = function universalLoader(req, res) {
   const filePath = path.resolve(__dirname, '..', 'build', 'index.html')
 
-  process.env.HOST = req.headers.host
-  process.env.PROTOCOL = req.protocol
-
-  fs.readFile(filePath, 'utf8', (err, htmlData)=>{
-    if (err) {
-      console.error('read err', err)
-      return res.status(404).end()
-    }
-    const context = {}
-    const store = configureStore()
-    const markup = renderToString(
-      <Provider store={store}>
-        <StaticRouter
-          location={req.url}
-          context={context}
-        >
-          <App/>
-        </StaticRouter>
-      </Provider>
-    )
-    const regHead = /<head[^>]*>[\s\S]*<\/head>/gi
-    const helmet = Helmet.renderStatic()
-    const originHead = htmlData.match(regHead)[0]
-    let html = htmlData.replace(regHead, `
+  const context = {}
+  const store = configureStore()
+  const markup = renderToString(
+    <Provider store={store}>
+      <StaticRouter
+        location={req.url}
+        context={context}
+      >
+        <App/>
+      </StaticRouter>
+    </Provider>
+  )
+  const helmet = Helmet.renderStatic()
+  const html = `
+    <!doctype html>
+    <html ${helmet.htmlAttributes.toString()}>
       <head>
         ${helmet.title.toString()}
         ${helmet.meta.toString()}
         ${helmet.link.toString()}
-        ${originHead.replace('<head>', '').replace('</head>', '')}
       </head>
-    `)
-    html = html.replace('<html>', `<html ${helmet.htmlAttributes.toString()}>`)
-    html = html.replace('<body>', `<body ${helmet.bodyAttributes.toString()}>`)
+      <body ${helmet.bodyAttributes.toString()}>
+        <div id="app">${markup}</div>
+      </body>
+    </html>
+  `
 
-    if (context.url) {
-      // Somewhere a `<Redirect>` was rendered
-      res.redirect(301, context.url)
-    } else {
-      res.send(html)
-    }
-  })
+  if (context.url) {
+    // Somewhere a `<Redirect>` was rendered
+    res.redirect(301, context.url)
+  } else {
+    res.send(html)
+  }
 }
 
